11cc1f9e357dba0c85086d7c1262686c993c7350
vinhnguyen <thanhvinh532005@gmail.com>
2024-02-29 02:44:47 -0600

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,35 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+=======
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Chatbot from "./chatbot/Chatbot";
+
+const router = createBrowserRouter([
+  {
+    path: "/chatbot",
+    element: <Chatbot />,
+  },
+]);
+>>>>>>> Stashed changes
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+<<<<<<< Updated upstream
     <App />
+=======
+    <RouterProvider router={router} />
+>>>>>>> Stashed changes
   </React.StrictMode>
 );
 
